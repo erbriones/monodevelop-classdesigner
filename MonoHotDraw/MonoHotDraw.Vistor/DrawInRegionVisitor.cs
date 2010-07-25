3e2027794203f69c8162ff4c37885fbfbb5e1406
erbriones <erbriones@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-07-25 21:30:53 +0000

    Added new Visitor namespace
    git-svn-id: https://mono-soc-2010.googlecode.com/svn/trunk/ClassDesigner@97 a470b8cb-0e6f-1642-1b45-71e107334c4b

---
+++ MonoHotDraw/MonoHotDraw.Vistor/DrawInRegionVisitor.cs
@@ -0,0 +1,86 @@
+// 
+// DrawFigureVisitor.cs
+//  
+// Author:
+//       Evan Briones <erbriones@gmail.com>
+// 
+// Copyright (c) 2010 Evan Briones
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoHotDraw.Figures;
+using MonoHotDraw.Handles;
+using MonoHotDraw.Util;
+
+namespace MonoHotDraw.Visitor
+{
+	public class DrawInRegionVisitor : IFigureVisitor
+	{		
+		Cairo.Context context;
+		Gdk.Region region;
+		IDrawingView view;
+		List<IFigure> figures;
+		
+		public DrawInRegionVisitor (Gdk.Region region, Cairo.Context context, IDrawingView view)
+		{
+			this.context = context;
+			this.region = region;
+			this.view = view;
+			this.figures = new List<IFigure> ();
+		}
+		
+		public IEnumerable<IFigure> AffectedFigures { 
+			get { return figures; }
+		}
+		
+		#region IFigureVisitor implementation		
+		public void VisitFigure (IFigure figure)
+		{
+			var point = view.DrawingToView (figure.DisplayBox.X, figure.DisplayBox.Y);
+			var rect = new RectangleD (point.X, point.Y);
+			rect.Width = figure.DisplayBox.Width;
+			rect.Height = figure.DisplayBox.Height;
+			
+			if (!GdkCairoHelper.RectangleInsideGdkRegion (rect, region))
+				return;
+			
+			if (view.SelectionEnumerator.Contains (figure))
+				figure.DrawSelected (context);
+			else
+				figure.Draw (context);
+			
+			figures.Add (figure);
+		}
+		
+		public void VisitHandle (IHandle handle)
+		{
+			if (view.SelectionEnumerator.Contains (handle.Owner)) {
+				handle.Draw (context, view);
+
+				foreach (IHandle childHandles in handle.Owner.Handles)
+					childHandles.Draw (context, view);
+			}
+		}
+		#endregion
+	}
+}
